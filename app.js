7ee9b0bd6e47236aeb349248eb349bc825013d06
Eric Ebersohl <eric.ebersohl.1@gmail.com>
2016-07-18 16:40:39 -0500

    Fixing the Initial Commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config();
 
 var express = require('express');
@@ -38,44 +37,3 @@ app.use('/database',database);
 //listen to a port
 app.listen(3000);
 console.log("Listening to port 3000");
-=======
-require('dotenv').config();
-
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-
-//var api = require('./routes/api'); Not sure if api is still relevant
-var account = require('./routes/account')
-var database = require('./routes/database');
-var index = require('./routes/index');
-var games = require('./routes/games');
-var characters = require('./routes/characters');
-
-//mongoose
-mongoose.connect(process.env.DB_CONNECTION);
-
-//verify mongoose connection
-mongoose.connection.on('connected',function(){
-  console.log('Mongoose connected');
-});
-
-//setup ejs
-app.set('view engine','pug');
-
-//static files
-app.use(express.static('./public'));
-
-//routes
-//app.use('/api',api); Not sure if api is still relevant
-
-app.use('/', index);
-app.use('/account', account);
-app.use('/games', games);
-app.use('/characters', characters);
-app.use('/database',database);
-
-//listen to a port
-app.listen(3000);
-console.log("Listening to port 3000");
->>>>>>> cac6cef050b1a472a9382bdd6396773bc3af74e8
--- routes/games.js
+++ routes/games.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
@@ -7,13 +6,3 @@ router.get('/',function(req,res){
 });
 
 module.exports = router;
-=======
-var express = require('express');
-var router = express.Router();
-
-router.get('/',function(req,res){
-  res.render('games');
-});
-
-module.exports = router;
->>>>>>> cac6cef050b1a472a9382bdd6396773bc3af74e8
